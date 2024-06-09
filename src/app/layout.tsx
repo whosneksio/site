e44e144918c79e01b583c652a09c885f15e0d9e8
whosneksio <MARPLIC10@edu.gdansk.pl>
2024-06-09 18:37:17 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from "next";
+import type { Viewport } from 'next'
+
 import { Inter } from "next/font/google";
-import Head from 'next/head'; // Import Head component from next/head
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+ 
+export const viewport: Viewport = {
+  themeColor: '#D86A59',
+}
 
 export const metadata: Metadata & { url: URL } = {
   title: 'resonance.rest',
   description: 'Discover a range of innovative tools designed to enhance your experience with Wuthering Waves. Our projects are open-source, fostering collaboration and community-driven development.',
   keywords: 'resonance, rest, api, image, avatar, generation, simple, fast, reliable, ww, wuthering waves, wuwa, wuthering, wutheringwwaves, waves, wuwaapi, wwapi, icon, api, wuthering waves fps unlocker, fps unlocker, fps, fps unlock, wuthering waves rpc, wuthering waves discord rich presence, rpc, discord rich presence',
-  themeColor: '#D86A59',
   url: new URL('https://resonance.rest/'), 
   openGraph: {
     title: 'resonance.rest',
