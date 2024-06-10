fbbf6b4077511cb8b1b8bd2d1d1511129532ed0f
whosneksio <MARPLIC10@edu.gdansk.pl>
2024-06-10 22:05:49 +0200

    phone optimize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-cover bg-no-repeat" style={{ backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('https://cdn.resonance.rest/bg.png')" }}>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-cover bg-no-repeat p-4 sm:p-6" style={{ backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('https://cdn.resonance.rest/bg.png')" }}>
       <img src="https://cdn.resonance.rest/logo.png" width="200" height="200" alt="Logo" />
-      <h1 className="mt-4 text-zinc-50 text-2xl">resonance.rest</h1>
-      <p className="mt-2 text-center text-zinc-400 max-w-md">
+      <h1 className="mt-4 text-zinc-50 text-2xl sm:text-3xl">resonance.rest</h1>
+      <p className="mt-2 text-center text-zinc-400 max-w-xs sm:max-w-md">
         Discover a range of innovative tools designed to enhance your experience with Wuthering Waves. Our projects are open-source, fostering collaboration and community-driven development.
       </p>
       <ul className="mt-4 text-zinc-300 text-center text-lg">
